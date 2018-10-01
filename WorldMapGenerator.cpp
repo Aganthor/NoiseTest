75d2dfd8109b9fbfeb9eae85d830a250c603df1d
Aganthor <psyluche@gmail.com>
2018-09-30 20:56:32 -0400

    Removed base generator class.

--- WorldMapGenerator.cpp
+++ WorldMapGenerator.cpp
@@ -1,8 +1,24 @@
 #include "WorldMapGenerator.h"
 
-namespace mapgenerator
+namespace map
 {
-	WorldMapGenerator::WorldMapGenerator(int width, int height) : BaseMapGenerator(width, height)
+	WorldMapGenerator::WorldMapGenerator() : m_mapWidth{ 0 }, m_mapHeight{ 0 }, m_seed{ 0 },
+		m_scale{ 0.0f }, m_octaves{ 0 }, m_persistance{ 0.0f }, m_lacunarity{0.0f}
+	{
+
+	}
+	
+	WorldMapGenerator::WorldMapGenerator(int width, int height, int seed, 
+		float scale, int octaves,
+		float persistance, float lacunarity) : 
+		m_mapWidth{ width }, m_mapHeight{ height }, 
+		m_seed{ seed }, m_scale{ scale }, 
+		m_octaves{ octaves }, m_persistance{ persistance }, 
+		m_lacunarity{ lacunarity }
+	{
+
+	}
+	WorldMapGenerator::WorldMapGenerator(int width, int height) : m_mapWidth{ width }, m_mapHeight{ height }
 	{
 		m_mapData.resize(m_mapWidth * m_mapHeight);
 	}
@@ -12,16 +28,18 @@ namespace mapgenerator
 
 	}
 
-	void WorldMapGenerator::generateMap()
+	void WorldMapGenerator::generateMap(int seed, float scale, int octaves, float persistance, float lacunarity)
 	{
 		//Use simplex noise to generate our map.
 		m_myNoise.SetNoiseType(FastNoise::Simplex);
 
-		for (auto w = 0; w < m_mapWidth; ++w)
+		//m_myNoise.SetFrequency(0.005);
+
+		for (auto x = 0; x < m_mapWidth; ++x)
 		{
-			for (auto h = 0; h < m_mapHeight; ++h)
+			for (auto y = 0; y < m_mapHeight; ++y)
 			{
-				m_mapData[h * m_mapHeight + w] = m_myNoise.GetNoise(w, h);
+				m_mapData[y * m_mapWidth + x] = m_myNoise.GetNoise(x, y);
 			}
 		}
 	}
