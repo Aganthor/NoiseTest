1d284fb529798f83aa85b67ab6438e17c8ecc8f4
Aganthor <psyluche@gmail.com>
2018-10-05 20:47:57 -0400

    Tiletype information.

---
+++ map/TileType.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace map
+{
+	//
+	//  An enum to specify the diffent tiles that can exist in the game.
+	//
+	enum class TileType
+	{
+		DEEP_WATER,
+		SHALLOW_WATER,
+		SHORE,
+		SAND,
+		GRASS,
+		FOREST,
+		DIRT,
+		ROCK,
+		MOUNTAIN,
+		SNOW,
+		SNOWY_MOUNTAIN
+	};
+}
